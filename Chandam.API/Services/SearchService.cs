7980290a785a1cb492d99fa5065b6cec7ef8434d
Dileep Miriyala <m.dileep@gmail.com>
2026-04-22 11:02:39 -0400

    Revert Non Code changes Signed-off-by: Dileep Miriyala <m.dileep@gmail.com>

--- Chandam.API/Services/SearchService.cs
+++ Chandam.API/Services/SearchService.cs
@@ -305,11 +305,11 @@ namespace Chandam.API.Services
 
     public class RuleSummaryDetailed
     {
-        public string Identifier { get; set; } = string.Empty;
-        public string Name { get; set; } = string.Empty;
-        public string PadyamType { get; set; } = string.Empty;
-        public string PadyamSubType { get; set; } = string.Empty;
-        public string Frequency { get; set; } = string.Empty;
+        public string Identifier { get; set; }
+        public string Name { get; set; }
+        public string PadyamType { get; set; }
+        public string PadyamSubType { get; set; }
+        public string Frequency { get; set; }
         public int Lines { get; set; }
         public string? ChandamName { get; set; }
         public int? CharLength { get; set; }
